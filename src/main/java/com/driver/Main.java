5d4d6c902af5dea50c94ebe44b26515f1a38c2aa
despo <shhripadb.20@gmail.com>
2023-09-14 18:09:09 +0530

    second  attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,8 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         RWOnly rwOnly=new RWOnly();
-        rwOnly.name="shri";
-        System.out.println(rwOnly.name);
+       // rwOnly.name="shri";
+       // System.out.println(rwOnly.name);
         //java: name has private access in com.driver.RWOnly
         rwOnly.setName("shri");
         rwOnly.getName();
